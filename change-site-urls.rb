97019199eff7f670d53aeb758e78106eddc6f5f4
david mz <david.mazvovsky@gmail.com>
2013-11-01 09:17:04 -0700

    updates

--- change-site-urls.rb
+++ change-site-urls.rb
@@ -5,7 +5,8 @@ config = YAML.load(File.read('_config.yml'))
 p "Loaded the file"
 
 config['url'] = "http://#{ARGV[0]}"
-config['post_url'] = "http://#{ARGV[1]}/lead"
+config['post_url'] = "http://data.#{ARGV[0]}/lead"
+config['domain'] = "#{ARGV[0]}"
 
 File.open('_config.yml','w') { |f| f.write config.to_yaml }
 
--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@ git reset --hard
 git pull
 
 site_domain=`printf '%s\n' "${PWD##*/}"`
-ruby change-site-urls.rb $site_domain data.$site_domain
+ruby change-site-urls.rb $site_domain
 
 jekyll build
