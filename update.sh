95474e2c1bd9cb2dbb36b2a326be43f3a0c9cdff
david mz <david.mazvovsky@gmail.com>
2013-11-01 07:30:22 -0700

    test see if it works

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
-git reset --hard
-git pull
-
-site_domain = $(printf '%s\n' "${PWD##*/}")
-
+#git reset --hard
+#git pull
+site_domain=`printf '%s\n' "${PWD##*/}"`
+echo $site_domain
 ruby change-site-urls.rb $site_domain api.$site_domain
